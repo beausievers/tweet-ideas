7bf9093cb3494e4f913be934d5303279e2e4f9cc
Beau Sievers <beau@beausievers.com>
2016-08-30 12:05:40 -0400

    Added fortunes; added default topics and fortunes files

--- tweet.py
+++ tweet.py
@@ -3,16 +3,42 @@
 import random
 
 
+def lines_from_file(path, n=1):
+    """Return n lines from file at path."""
+    with open(path, 'r') as f:
+        topics = f.readlines()
+    lines = [random.choice(topics) for _ in range(n)]
+    return lines
+
+
 def tweet_ideas(config_path, n=1):
     """Return n tweet topics from config_path."""
-    with open(config_path, 'r') as config:
-        topics = config.readlines()
-    selected_topics = [random.choice(topics) for _ in xrange(n)]
-    print("You could tweet about...")
+    selected_topics = lines_from_file(config_path, n)
+    print("You could tweet...")
     for topic in selected_topics:
         print_topic(topic)
+
+
+def fortune(fortune_path, n=1):
+    """Return n fortunes from fortune_path."""
+    fortunes = lines_from_file(fortune_path, n)
+    print("Sound advice for quality tweets:")
+    for fortune in fortunes:
+        print_fortune(fortune)
 
 
 def print_topic(topic):
     """Print a single tweet idea."""
     print("    ..." + topic.strip())
+
+
+def print_fortune(fortune):
+    """Print a single fortune."""
+    print(fortune)
+
+
+if __name__ == '__main__':
+    print(' ')
+    tweet_ideas('topics.txt', n=1)
+    print(' ')
+    fortune('fortunes.txt', n=1)
